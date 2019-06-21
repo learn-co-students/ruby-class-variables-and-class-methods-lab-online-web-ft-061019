ab449643d2efb5b6104f7d40e7929bd4c07cfd4a
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-06-21 14:25:30 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -7,6 +7,7 @@ class Song
   end 
   @@genres = []
   @@artists = []
+  
   def initialize(name, artist, genres)
     @name = name 
     @artist = artist
@@ -14,10 +15,19 @@ class Song
     @@count +=1
     @@genres << @genre
     @@artists << @artist
+    #@@genre_count +=1
   end 
-  def self.artist
-  @artits.uniq
+  def self.artists
+  @@artists.uniq
   end
-  
+  def self.genres
+    @@genres.uniq
+  end 
+  def self.genre_count
+    @@genre_count = {}
+    @@genres each do |genre| 
+  end 
+  def self.artist_count
+  end 
 #binding.pry
 end 
